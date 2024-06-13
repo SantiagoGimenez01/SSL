5f83f598b36d3d31818be744053733f1787e9c2c
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-06-13 19:42:13 -0300

    Primer avance de implementacion_1

--- 02-MaquinasDeEstado/histograma-1-enum-switch.c
+++ 02-MaquinasDeEstado/histograma-1-enum-switch.c
@@ -1,7 +1,26 @@
 #include "histograma.h"
 
 
-int histogramaImplementacion1 (){
+typedef enum{
+    IN, //Dentro de la palabra
+    OUT //Fuera de la palabra
+} State;
 
-    return 0;
+int* histograma_enum_switch(const char* texto){
+    State estado = OUT; //Comenzamos con el estado fuera de la palabra
+    int histograma[10]; // Vector para guardar cantidad de palabras segun largo (max largo = 10)
+    int caracter; // Variable para contar caracteres
+    char c; //Variable para leer caracteres del texto
+    while((c = texto[caracter]) != '\0'){
+        switch(estado){
+            case OUT:
+                caracter = 0;
+                if(c != '\n' || c != " " || c != '\t')
+                    estado = IN;
+                ++caracter;
+                break;
+        }
+    }
+    
+
 }
